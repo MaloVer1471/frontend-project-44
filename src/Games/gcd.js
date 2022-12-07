9ef6587309dae8137603805a5301e06b6cb899d6
MaloVer1471 <ig.lager@yandex.ru>
2022-12-07 10:13:56 +0300

    Step 7 - end

---
+++ src/Games/gcd.js
@@ -0,0 +1,34 @@
+import gameBase from '../gameComparison.js';
+import randomNumb from '../randomNumb.js';
+
+const gcd = () => {
+  const gcdNote = 'Find the greatest common divisor of given numbers.';
+
+  const gcdTask = () => {
+    const num1 = randomNumb(100);
+    const num2 = randomNumb(100);
+
+    const quest = `${num1} ${num2}`;
+
+    const gcdGet = (numb1, numb2) => {
+        let number1 = numb1;
+        let number2 = numb2;
+        while (number1 !== 0 && number2 !== 0) {
+          if (number1 > number2) {
+            number1 %= number2;
+          } else {
+            number2 %= number1;
+          }
+        }
+        return (number1 + number2);
+      };
+
+    const result = gcdGet(num1, num2).toString();
+
+    return [quest, result];
+  };
+
+  gameBase(gcdNote, gcdTask);
+};
+
+export default gcd;
